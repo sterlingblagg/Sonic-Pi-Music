c9c4961d2912ce8b6965111cdc1f2d59cb092773
Sterling Blagg <37843170+sterlingblagg@users.noreply.github.com>
2020-03-12 13:20:04 -0700

    Add files via upload

--- Custom Song 2/come_together6.rb
+++ Custom Song 2/come_together6.rb
@@ -1,5 +1,6 @@
 use_synth :piano
 use_bpm 88
+x = 0
 
 define :repeat do |a, b, c, d, e, f, g|
   play a
@@ -102,60 +103,63 @@ define :part3 do |a, b, c, d|
   play b, sustain: 2
   play c, sustain: 2
   sleep d
-end
-
-
-define :dnote do
-  play :d2
-  sleep 1
-end
-
-define :anote do
-  play :a2
-  sleep 1
-end
-#################The Song.#################
-4.times do
-  repeat :d2, :d2, :g2, :a2, :c3, :f3, :a2
-end
-
-#play1
-live_loop :note1 do
-  16.times do
-    dnote
+  x + 1
+  if x = 2
+    d = 1
   end
-  stop
-end
-
-measureintro :f4, :c4, :f4, :c4, :f4, :c4, :f4, :c4, :d4, :c4, :d4
-#play2
-measurerepeat :f4, :c4, :f4, :c4, :f4, :c4, :d4, :c4, :d4, 1
-#play3
-measurerepeat :f4, :c4, :f4, :c4, :f4, :c4, :d4, :c4, :d4, 1
-#play4
-measurerepeat :f4, :c4, :f4, :c4, :f4, :c4, :d4, :c4, :d4, 0.5
-
-live_loop :note2 do
-  8.times do
-    anote
+  
+  
+  define :dnote do
+    play :d2
+    sleep 1
   end
-  stop
-end
-
-part2 :Db4, :e4, :Db4, :e4, :Db4, :e4, :Eb4, :c4, :Db4, :e4
-#play6
-part3 :g2, :b3, :f4, 2.5
-
-4.times do
-  repeat :d2, :d2, :g2, :a2, :c3, :f3, :a2
-end
-live_loop :note1 do
-  16.times do
-    dnote
+  
+  define :anote do
+    play :a2
+    sleep 1
   end
-  stop
-end
-measureintro :f4, :c4, :f4, :c4, :f4, :c4, :f4, :c4, :d4, :c4, :d4
-measurerepeat2 :f4, :c4, :d4, :f4, :c4, :d4, :c4, :d4
-measurerepeat2 :f4, :c4, :d4, :f4, :c4, :d4, :c4, :d4
-measurerepeat2 :f4, :c4, :d4, :f4, :c4, :d4, :c4, :d4
+  #################The Song.#################
+  4.times do
+    repeat :d2, :d2, :g2, :a2, :c3, :f3, :a2
+  end
+  
+  #play1
+  live_loop :note1 do
+    16.times do
+      dnote
+    end
+    stop
+  end
+  
+  measureintro :f4, :c4, :f4, :c4, :f4, :c4, :f4, :c4, :d4, :c4, :d4
+  #play2
+  measurerepeat :f4, :c4, :f4, :c4, :f4, :c4, :d4, :c4, :d4, 1
+  #play3
+  measurerepeat :f4, :c4, :f4, :c4, :f4, :c4, :d4, :c4, :d4, 1
+  #play4
+  measurerepeat :f4, :c4, :f4, :c4, :f4, :c4, :d4, :c4, :d4, 0.5
+  
+  live_loop :note2 do
+    8.times do
+      anote
+    end
+    stop
+  end
+  
+  part2 :Db4, :e4, :Db4, :e4, :Db4, :e4, :Eb4, :c4, :Db4, :e4
+  #play6
+  part3 :g2, :b3, :f4, 2.5
+  
+  4.times do
+    repeat :d2, :d2, :g2, :a2, :c3, :f3, :a2
+  end
+  live_loop :note1 do
+    16.times do
+      dnote
+    end
+    stop
+  end
+  measureintro :f4, :c4, :f4, :c4, :f4, :c4, :f4, :c4, :d4, :c4, :d4
+  measurerepeat2 :f4, :c4, :d4, :f4, :c4, :d4, :c4, :d4
+  measurerepeat2 :f4, :c4, :d4, :f4, :c4, :d4, :c4, :d4
+  measurerepeat2 :f4, :c4, :d4, :f4, :c4, :d4, :c4, :d4
